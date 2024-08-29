a27348abeeea3e52f0fa4a0e6c3cf1eb7047ce99
KatouHirosato <96373092+KatouHirosato@users.noreply.github.com>
2024-08-29 19:06:55 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,17 +5,19 @@ from . import selectduty
 from . import notification
 
 def main():
-    DAY = datetime.datetime.now().weekday()
-    if DAY == 0:
+    day = datetime.datetime.now().weekday()
+    if day == 0:
         selectduty.reset()
         selectduty.normalize_per_people()
-    weight = selectduty.todays_weights(DAY)
+    weight = selectduty.todays_weights(day)
     persons = selectduty.select(weight)
     message = f"おはようございます。今日のシャッフルトークは\n{persons[0]}さん {persons[1]}さん {persons[2]}sさん\nです。良い一日を！"
     client_id = sys.argv[1]
     client_secret = sys.argv[2]
+    print(client_id,client_secret)
     notification.send_notification(client_id, client_secret, message)
 
 
 if __name__ == '__main__':
+    print('HERE2')
     main()
