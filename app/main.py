6d2861c9379db1e610b195af43dbee65f76f3e6f
KatouHirosato <96373092+KatouHirosato@users.noreply.github.com>
2024-08-29 19:08:58 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,10 +14,8 @@ def main():
     message = f"おはようございます。今日のシャッフルトークは\n{persons[0]}さん {persons[1]}さん {persons[2]}sさん\nです。良い一日を！"
     client_id = sys.argv[1]
     client_secret = sys.argv[2]
-    print(client_id,client_secret)
     notification.send_notification(client_id, client_secret, message)
 
 
 if __name__ == '__main__':
-    print('HERE2')
     main()
