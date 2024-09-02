216817f5fb46709b274056bc4796ea15165f09e3
KatouHirosato <96373092+KatouHirosato@users.noreply.github.com>
2024-09-02 10:47:10 +0900

    Update notification.py

--- app/notification.py
+++ app/notification.py
@@ -15,12 +15,7 @@ def add_greeting_to_message(input_message):
     # )
     # output_message = response['choices'][0]['message']['content'].strip()
     # return output_message
-    greetings = [
-        "よろしくお願いします。",
-        "良い朝を！",
-        "今日も頑張りましょう！",
-    ]
-    return input_message+numpy.random.choice(greetings)
+    return input_message+"よろしくお願いします。"
 
 def send_notification(client_id, client_secret, message):
     url = 'https://api.ovice.io/api/public/v1/organizations/notification'
