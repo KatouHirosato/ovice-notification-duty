30e234eb26fb7ac83ac3bfc7fe492e20afa3fd43
kh <kh@temp-07.garage.twinengine.jp>
2024-09-05 18:12:15 +0900

    TEST

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import datetime
+import jpholiday
 
 from . import selectduty
 from . import notification
@@ -10,11 +11,11 @@ CSVFILE = 'schedule.csv'
 
 def main():
     day = datetime.datetime.now().weekday()
-    if day == 0 or not os.path.exists(CSVFILE):
-        pass
-    download.download_sheet_as_csv(CSVFILE)
-    selectduty.normalize_per_person(CSVFILE)
-    
+    if day == 0:
+        download.download_sheet_as_csv(CSVFILE)
+        selectduty.normalize_per_person(CSVFILE)
+    if jpholiday.is_holiday(datetime.datetime.now().date()):
+        return
     persons = selectduty.select(day, CSVFILE)
     notification.send_notification(*persons)
 
